6182161f87f4c92efccd44074b26a4520a218b1d
Ankita3503 <94631478+Ankita3503@users.noreply.github.com>
2022-01-17 20:45:16 +0530

    Add files via upload

---
+++ DrawableInterface.java
@@ -0,0 +1,29 @@
+import java.awt.dnd.DropTarget;
+// Create The Interface
+interface Drawable{
+    void draw();
+}
+// We can Inherit the Interface
+class Ractangle implements Drawable{
+    @Override
+    public void draw() {
+        System.out.println("Ractangle is Draw.");
+    }
+}
+
+//Again inherit the interface
+class Circle implements Drawable{
+    @Override
+    public void draw() {
+        System.out.println("Circle is Draw.");
+    }
+}
+// This is our Public class
+public class DrawableInterface {
+    public static void main(String[] args) {
+        Drawable obj=new Circle();
+        obj.draw();
+        Drawable obj1=new Ractangle();
+        obj1.draw();
+    }
+}
