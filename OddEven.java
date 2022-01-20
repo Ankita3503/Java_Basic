7cacf823eb5f4ba0cc0d8efa5fa7bd296d8a34cc
Ankita3503 <94631478+Ankita3503@users.noreply.github.com>
2022-01-20 22:17:54 +0530

    Update OddEven.java

--- OddEven.java
+++ OddEven.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 
 public class OddEven {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the number:");
         int n= sc.nextInt();
-        if(n%2==0){
-            System.out.println("Entered no is even.");
-        }else{
-            System.out.println("Entered no is Odd.");
+        if(n%2==0)
+        {
+            System.out.println("Entered number  is even.");
+        }
+        else
+        {
+            System.out.println("Entered number is Odd.");
         }
     }
 }
